9ab22e4c22412d4284f56ab8609c86b072b6881f
alexvivror <alexvivror@gmail.com>
2026-08-11 12:29:14 +0000

    Fix: import AppNavigation in MainActivity

--- app/src/main/java/com/example/researchtovideo/ui/MainActivity.kt
+++ app/src/main/java/com/example/researchtovideo/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.researchtovideo.theme.ResearchToVideoTheme
+import com.example.researchtovideo.ui.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
